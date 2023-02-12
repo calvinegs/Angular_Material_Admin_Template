6b8e3734fd822a3a5c5d91b51aea7e6ab0ca1732
service.egs <service.egs@gmail.com>
2023-02-12 13:16:45 +0800

    ver.1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlexboxComponent } from './components/flexbox/flexbox.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'flexbox', component: FlexboxComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShareMaterialModule } from './shared/share-material.module';
+import { FlexboxComponent } from './components/flexbox/flexbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlexboxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ShareMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
